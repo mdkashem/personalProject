e726de4defa3809466bd03f5f351ee94bac10cd3
Md Abul Kashem <56566230+mdkashem@users.noreply.github.com>
2019-10-21 15:36:31 -0700

    update

---
+++ project/app/src/main/java/com/senior/alphabets/MainActivity.java
@@ -0,0 +1,50 @@
+package com.senior.alphabets;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Integer images[] = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d,R.drawable.e,R.drawable.f,R.drawable.g,R.drawable.h, R.drawable.i2, R.drawable.j,R.drawable.k,R.drawable.l,R.drawable.m,R.drawable.n,R.drawable.o,R.drawable.p,R.drawable.q,R.drawable.r,R.drawable.s,R.drawable.t,R.drawable.u,R.drawable.v,R.drawable.w,R.drawable.x,R.drawable.y,R.drawable.z};
+    private int currImage = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setInitialImage();
+        setImageRotateListener();
+    }
+
+    private void setImageRotateListener() {
+        final Button rotatebutton = (Button) findViewById(R.id.btnRotateImage);
+        rotatebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                currImage++;
+                if (currImage == 26) {
+                    currImage = 0;
+                }
+                setCurrentImage();
+            }
+        });
+    }
+
+    private void setInitialImage() {
+        setCurrentImage();
+    }
+
+    private void setCurrentImage() {
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imageDisplay);
+        imageView.setImageResource(images[currImage]);
+
+    }
+}
